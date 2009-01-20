e04acc54c6699b74fb6b75c7d1e2935e83268e56
vann <vann@innerfusion.net>
2009-01-20 17:29:06 -0600

    added css class override on link_to_box and updated readme

--- js/load_fancybox.js
+++ js/load_fancybox.js
@@ -1,3 +1,20 @@
+// Default fancybox loader
+// Will automatically attach fancybox to links with the following elements:
+//  - a.fancy-img
+//  - .gallery a
+//  - a.fancy
+
+// OPTIONS : source (http://fancy.klade.lv/)
+// hideOnContentClick : Hides FancyBox when cliked on zoomed item (false by default)
+// zoomSpeedIn : Speed in miliseconds of the zooming-in animation (no animation if 0)
+// zoomSpeedOut : Speed in miliseconds of the zooming-out animation (no animation if 0)
+// frameWidth : Default width for iframed and inline content
+// frameHeight : Default height for iframed and inline content
+// overlayShow : If true, shows the overlay (false by default)
+// overlayOpacity : Opacity of overlay (from 0 to 1)
+// itemLoadCallback : Custom function to get group items 
+//                   (see example on source of : http://fancy.klade.lv/)
+
 $(document).ready(function() {
   $("a.fancy-img").fancybox();
   $(".gallery a").fancybox();
--- lib/fancy_box.rb
+++ lib/fancy_box.rb
@@ -2,7 +2,7 @@ module FancyBox
   
   # includes all the files necessary to use fancy_box
   # just put <%= include_fancy_box %> in the head of your page
-  def include_fancy_box
+  def include_fancy_box(*args)
     content = javascript_include_tag('jquery.fancybox-1.0.0.js','jquery.pngFix.pack.js',                                      
                                      'load_fancybox')
     content << "\n#{stylesheet_link_tag('fancy')}"                                
@@ -13,9 +13,18 @@ module FancyBox
   #   link_to_box "Question", "#question" # => opens a div named question
   #   link_to_box "Google", "http://google.com" # => opens google in a fancy box
   #   link_to_box "About", "/about" # => opens the about page in a fancy_box
+  #   link_to_box "About", "/about", 
+  #               :title => "About Us" # => opens about page in a fancy_box with a a title
+  #   link_to_box "Special Box", "#special", :box_class => "small"
+  #       # => Opens #special with the class of 'small' assuming that you set it 
+  #            up in load_fancybox.js
   def link_to_box(content, link, options={})
-    options[:class] ||= ""
-    options[:class] << " fancy"
+    box_class = options[:box_class].nil? ? "fancy" : options[:box_class]
+    
+    options[:class] << " #{box_class}" unless options[:class].nil?
+    options[:class] = "#{box_class}" if options[:class].nil?
+    
+    options.delete :box_class
     link_to content, link, options
   end
 
@@ -36,6 +45,7 @@ module FancyBox
   #   fancy_gallery "first.jpg:A optional title", "second.jpg:A optional title"
   #   just pass in a list of strings and an optional title with a ":"
   #   please note: the title doesn't render properly if you are using a reset css file
+  #   NOTE : if you want to change the look of the gallery, just modify the .gallery style
   def fancy_gallery(*imgs)
     content = ""
     group_name = "group-#{random_id}"
@@ -49,8 +59,30 @@ module FancyBox
     content_tag(:div,content,:id => "gallery-#{random_id}",:class => "gallery")
   end
   
+  # creates an inline fancy_box with a title
+  # EX: 
+  #   fancy_box :title => "My Fancy Box" do
+  #     <p>Content in my fancy box</p> 
+  #   end
+  # OPTIONS:
+  #   :title = the title of for the fancy box
+  #   :title_tag = the tag for the title defaults to h3
+  #   :title_options = html options for the title tag
+  #   any other standard html options...
+  def fancy_box(options={}, &blk)
+    options[:title_tag] ||= :h3
+    options[:title_options] ||= {}
+    options[:style] << " display:none" unless options[:style].nil?
+    options[:style] = "display:none" if options[:style].nil?
+    title = content_tag(options[:title_tag], options[:title], options[:title_options])
+    content = options[:title].nil? ? "\n#{capture(&blk)}" : "\n#{title}\n#{capture(&blk)}"
+    %w(title title_options title_tag).each { |opt| options.delete opt.to_sym }
+    concat(content_tag(:div, content, options))
+  end
+  
   protected
     def random_id(num_hash=9999)
       rand(num_hash)
     end
+    
 end
